e164048ede342b7b7d5f5183609b6d56cc69aad5
Yoav Yosovich <yoav.yosovich@intl.att.com>
2023-02-12 09:59:26 +0200

    update baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://mern-server-omega.vercel.app",
         changeOrigin: true,
         secure: false,
       },
